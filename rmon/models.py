0504f7e8ac8ac0d4f22ea21cd66d35c1e73b01ec
chuang.li <chuang.li.eb@bp.renesas.com>
2017-12-20 15:49:07 +0900

    revise models.py

--- rmon/models.py
+++ rmon/models.py
@@ -10,6 +10,9 @@ from flask_sqlalchemy import SQLAlchemy
 from redis import StrictRedis, RedisError
 from datetime import datetime
 from rmon.common.rest import RestException
+from marshmallow import (Schema, fields, validate, post_load,
+                        validates_schema, ValidationError)
+
 
 db = SQLAlchemy()
 
@@ -62,3 +65,52 @@ class Server(db.Model):
             return self.redis.info()
         except RedisError:
             raise RestException(400, 'redis server %s can not connected' % self.host)
+
+
+class ServerSchema(Schema):
+    """Redis服务器记录序列化"""
+    id = fields.Integer(dump_only=True)
+    name = fields.String(required=True, validate=validate.Length(2, 64))
+    description = fields.String(validate=validate.Length(0, 512))
+    # 正则检验Ip v4 地址格式
+    host = fields.String(validate=validate.Regexp(r'^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$'))
+    port = fields.Integer(validate=validate.Range(1024, 65536))
+    password = fields.String()
+    updated_at = fields.DateTime(dump_only=True)
+    created_at = fields.DateTime(dump_only=True)
+
+    @validates_schema
+    def validate_schema(self, data):
+        """验证是否存在同名redis服务器"""
+        if 'port' not in data:
+            data['port'] = 6379
+
+        instance = self.context.get('instance', None)
+
+        server = Server.query.filter_by(name=data['name']).first()
+
+        if server is None:
+            return
+
+        # 更新服务器时
+        if instance is not None and server != instance:
+            raise ValidationError('Redis server already exist', 'name')
+
+        # 创建服务器时
+        if instance is None and server:
+            raise ValidationError('Redis server already exist', 'name')
+
+    @post_load
+    def create_or_update(self, date):
+        """数据加载成功后自动创建Server对象"""
+        instance = self.context.get('instance', None)
+
+        # 创建Redis服务器
+        if instance is None:
+            return Server(**data)
+
+        # 更新服务器
+        for key in data:
+            setattr(instance, key, data[key])
+        return instance
+
